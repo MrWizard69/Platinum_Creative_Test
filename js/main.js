15e70b49d16df42301a0e7fef88b9a23b2294347
Jordan Wilson <JordanWilson@fullsail.edu>
2015-06-16 20:55:17 -0400

    working on the carousel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+  $("#owls").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+  });
+ 
+});
