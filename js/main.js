a688679940d524185de59009019d825f05e1febd
Jordan Wilson <JordanWilson@fullsail.edu>
2015-06-16 22:46:21 -0400

    got a working carousel

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-$(document).ready(function() {
- 
-  $("#owls").owlCarousel({
- 
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true
- 
-  });
- 
-});
+$(document).ready(function()
+		{
+			$('#slider1').tinycarousel();
+		});
